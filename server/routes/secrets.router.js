c36e8d2285d3ce140a3a7da300aa9506c94fda6a
Thomas Rohde <trohde@rocketmail.com>
2021-02-02 15:20:19 -0600

    started adding fencing in router.get and altering pool.query

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -4,15 +4,20 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // what is the value of req.user????
-  console.log('req.user:', req.user);
+  if(req.isAuthenticated()){
 
-  pool
-    .query(`SELECT * FROM "secret";`)
+  console.log('req.user:', req.user);
+  
+  pool.query`SELECT * FROM "secret" WHERE "secrecy_level" < ${req.user.clearance_level};`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
+  } else {
+    res.sendStatus(403);
+  }
 });
+
 
 module.exports = router;
