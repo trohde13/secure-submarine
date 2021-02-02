f9396c4e7260298f9ec3253ff8f8ebb5048b8b8d
Thomas Rohde <trohde@rocketmail.com>
2021-02-02 15:31:58 -0600

    base is completed

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 
   console.log('req.user:', req.user);
   
-  pool.query`SELECT * FROM "secret" WHERE "secrecy_level" < ${req.user.clearance_level};`)
+  pool.query(`SELECT * FROM "secret" WHERE "secrecy_level" < ${req.user.clearance_level};`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
